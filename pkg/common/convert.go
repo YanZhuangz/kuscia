1e639e3c3e7f2cb582d7b32edfca9ba2c7083eb0
unknown <smile_yanzhuang@163.com>
2024-04-13 17:43:41 +0800

    update go file to test golangci-lint

--- pkg/common/convert.go
+++ pkg/common/convert.go
@@ -247,5 +247,6 @@ func (protocol *Protocol) UnmarshalYAML(unmarshal func(interface{}) error) error
 // test ci
 func TestCi() error {
 	domainId := "test-ci-123"
+	domainId = "test-ci"
 	return nil
 }
